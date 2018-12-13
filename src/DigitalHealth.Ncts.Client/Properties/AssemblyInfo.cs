cc3452f3cbef48a95d236201b19a886a9427d737
philip.wilford <philip.wilford@digitalhealth.gov.au>
2018-12-13 16:24:36 +1000

    Updated Newtonsoft to latest release.

--- src/DigitalHealth.Ncts.Client/Properties/AssemblyInfo.cs
+++ src/DigitalHealth.Ncts.Client/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NctsClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NctsClient")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Australian Digital Health Agency")]
 [assembly: AssemblyProduct("NctsClient")]
