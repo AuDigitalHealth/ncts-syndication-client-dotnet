452cb0254ce4394c39133f589a5a5da348de36d6
philip.wilford <philip.wilford@digitalhealth.gov.au>
2019-01-30 08:55:12 +1000

    Linked to nuget now.

--- src/DigitalHealth.Ncts.Client.Sample/Properties/AssemblyInfo.cs
+++ src/DigitalHealth.Ncts.Client.Sample/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NctsClient.Sample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DigitalHealth.Ncts.Client.Sample")]
+[assembly: AssemblyDescription("A library for connecting and downloading content from NCTS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Australian Digital Health Agency")]
-[assembly: AssemblyProduct("NctsClient.Sample")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("DigitalHealth.Ncts.Client.Sample")]
+[assembly: AssemblyCopyright("Copyright ©2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
